1129ec336ac51c34e2877efe0723e5f30614a534
Lucas Eskudlark <62945519+LucasEskudlark@users.noreply.github.com>
2024-05-17 19:56:19 -0300

    feat: add exception

---
+++ Testeger/Testeger/Shared/Exceptions/TestRequestNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿namespace Testeger.Shared.Exceptions;
+
+public class TestRequestNotFoundException : Exception
+{
+    public TestRequestNotFoundException()
+    {
+    }
+
+    public TestRequestNotFoundException(string message)
+        : base(message)
+    {
+    }
+
+    public TestRequestNotFoundException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+}
