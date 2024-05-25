96c459a76ced9efdf68f4cbfdc53aee0254c32a6
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-05-25 16:49:50 -0300

    feat: add breadcrumb helper

---
+++ Testeger/Shared/Helpers/BreadCrumbHelper.cs
@@ -0,0 +1,10 @@
+﻿using Testeger.Shared.Models.Entities;
+
+namespace Testeger.Shared.Helpers;
+
+public static class BreadCrumbHelper
+{
+    public static string GetProjectUrl(TestRequest request) => $"/project/{request.ProjectId}";
+    public static string GetRequestUrl(TestRequest request) => $"/test-request/{request.Id}";
+    public static string GetRequestText(TestRequest request) => $"Request N°{request.Number}";
+}
