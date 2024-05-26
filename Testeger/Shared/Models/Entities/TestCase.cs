653d6117e920eaae59fcd0faa5ca0aaddb114adb
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-05-26 17:08:07 -0300

    chore: add missing properties

--- Testeger/Shared/Models/Entities/TestCase.cs
+++ Testeger/Shared/Models/Entities/TestCase.cs
@@ -5,6 +5,11 @@ namespace Testeger.Shared.Models.Entities;
 
 public class TestCase
 {
+    public TestCase()
+    {
+        SavedTimes = new List<string> ();
+        ElapsedTime = TimeSpan.Zero;
+    }
     public string? Id { get; set; }
     public string? RequestId { get; set; }
     public string? CreatedBy { get; set; }
@@ -15,4 +20,8 @@ public class TestCase
     public DateTime? CompletedDate { get; set; }
     public TestCaseStatus Status { get; set; }
     public bool NeedCorrection { get; set; }
+    public List<string> SavedTimes { get; set; }
+    public bool IsCompleted { get; set; }
+    public TimeSpan ElapsedTime { get; set; }
+    public TimeSpan AmountOfTimeSpentToTest { get; set; }
 }
