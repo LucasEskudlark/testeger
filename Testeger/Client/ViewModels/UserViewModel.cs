c4f340bcf3d2aa7b34344886b6c4531a79d67bfb
Lucas Eskudlark <62945519+LucasEskudlark@users.noreply.github.com>
2024-05-27 18:28:40 -0300

    chore: create user view model

---
+++ Testeger/Client/ViewModels/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Testeger.Shared.Models.Enumerations;
+
+namespace Testeger.Client.ViewModels;
+
+public class UserViewModel
+{
+    [Required(ErrorMessage = "E-mail is required")]
+    [EmailAddress(ErrorMessage = "Needs to be a valid e-mail")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Role is required")]
+    [EnumDataType(typeof(RoleType))]
+    public RoleType Role { get; set; }
+}
