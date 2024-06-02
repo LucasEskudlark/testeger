1aa7a0a7fc3d62b9ef0425761d6c31efcb9292ea
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-06-02 18:58:52 -0300

    refact: change request history to a list

--- Testeger/Shared/Models/ValueObjects/TestRequestHistory.cs
+++ Testeger/Shared/Models/ValueObjects/TestRequestHistory.cs
@@ -4,10 +4,9 @@ namespace Testeger.Shared.Models.ValueObjects;
 
 public class TestRequestHistory
 {
-    public string? Id { get; set; }
     public string? TestRequestId { get; set; }
     public string? ChangedByUserId { get; set; }
-    public RequestStatus OldStatus { get; set; }
+    public RequestStatus? OldStatus { get; set; }
     public RequestStatus NewStatus { get; set; }
     public DateTime ChangedDate { get; set; }
 }
