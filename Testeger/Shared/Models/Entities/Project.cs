2528ac89434b0b27d9a2c3000c382d36d3dae67d
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-06-04 09:40:24 -0300

    chore: add id to user

--- Testeger/Shared/Models/Entities/Project.cs
+++ Testeger/Shared/Models/Entities/Project.cs
@@ -4,7 +4,7 @@ public class Project
 {
     public string? Id { get; set; }
     public string? Name { get; set; }
-    public IEnumerable<User>? Members { get; set; }
+    public IEnumerable<User> Members { get; set; }
     public DateTime? CreatedDate { get; set; }
     public string? CreatorId { get; set; }
 }
