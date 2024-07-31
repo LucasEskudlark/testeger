5f8c032ed58800666d49a0c2c455bf59899b0695
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-07-31 19:34:28 -0300

    feat: add enumerations

---
+++ Testeger/Testeger.Domain/Enumerations/PriorityLevel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace Testeger.Shared.Models.Enumerations;
+
+public enum PriorityLevel
+{
+    [Description("Low priority - can be addressed at a later time.")]
+    Low = 0,
+
+    [Description("Medium priority - should be addressed in due time.")]
+    Medium = 1,
+
+    [Description("High priority - requires prompt attention.")]
+    High = 2,
+
+    [Description("Urgent priority - needs immediate attention.")]
+    Urgent = 3
+}
