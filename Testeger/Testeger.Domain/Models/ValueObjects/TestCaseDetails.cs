c79e65922c7e5941549588b63ec2854fdf387855
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-07-31 19:34:51 -0300

    feat: add value objects

---
+++ Testeger/Testeger.Domain/Models/ValueObjects/TestCaseDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace Testeger.Shared.Models.ValueObjects;
+
+public class TestCaseDetails
+{
+    public string? Description { get; set; }
+    public string? PreConditions { get; set; }
+    public string? Steps { get; set; }
+    public string? ExpectedResult { get; set; }
+    public string? Environment { get; set; }
+}
