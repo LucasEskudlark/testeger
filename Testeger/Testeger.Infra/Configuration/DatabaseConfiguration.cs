a16034bb3444c3896dd6ccfbd3c1b4f5aae7f80f
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-08-02 19:48:04 -0300

    feat: add db configuration

---
+++ Testeger/Testeger.Infra/Configuration/DatabaseConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Testeger.Infra.Context;
+
+namespace Testeger.Infra.Configuration;
+
+public static class DatabaseConfiguration
+{
+    public static void AddDatabase(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnectionString");
+
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+    }
+}
---
+++ Testeger/Testeger.Infra/Configuration/RepositoriesConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Testeger.Infra.Repositories;
+using Testeger.Infra.Repositories.Images;
+using Testeger.Infra.Repositories.Projects;
+using Testeger.Infra.Repositories.TestCaseResults;
+using Testeger.Infra.Repositories.TestCases;
+using Testeger.Infra.Repositories.TestRequests;
+
+namespace Testeger.Infra.Configuration;
+
+public static class RepositoriesConfiguration
+{
+    public static void AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+        services.AddScoped<IProjectRepository, ProjectRepository>();
+        services.AddScoped<ITestCaseRepository, TestCaseRepository>();
+        services.AddScoped<ITestRequestRepository, TestRequestRepository>();
+        services.AddScoped<ITestCaseResultRepository, TestCaseResultRepository>();
+        services.AddScoped<IImageRepository, ImageRepository>();
+    }
+}
