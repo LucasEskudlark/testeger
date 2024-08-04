63558c4d01a861e20b61e4c3725cea85228a7824
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-08-03 22:47:47 -0300

    feat: add controller

---
+++ Testeger/Api/Controllers/ProjectsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Testeger.Application.Requests.CreateProject;
+using Testeger.Application.Services.Project;
+
+namespace Testeger.Api.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class ProjectsController : ControllerBase
+{
+    private readonly IProjectService _projectService;
+
+    public ProjectsController(IProjectService projectService)
+    {
+        _projectService = projectService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateProjectAsync([FromBody]CreateProjectRequest request)
+    {
+        var response = await _projectService.CreateProject(request);
+
+        return Ok(response);
+    }
+}
