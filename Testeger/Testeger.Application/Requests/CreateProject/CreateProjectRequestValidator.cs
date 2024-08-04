d9cf96a574b45a4bd013fb8d2f6267d9057974b9
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-08-03 22:46:24 -0300

    feat: add request and its validator

---
+++ Testeger/Testeger.Application/Requests/CreateProject/CreateProjectRequestValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Testeger.Application.Requests.CreateProject;
+
+public class CreateProjectRequestValidator : AbstractValidator<CreateProjectRequest>
+{
+    public CreateProjectRequestValidator()
+    {
+        RuleFor(r => r.Name)
+            .NotEmpty()
+            .MaximumLength(50);
+
+        RuleFor(r => r.UserId)
+            .NotEmpty()
+            .MaximumLength(36);
+    }
+}
