38c7b7adb35ad0cdbb7900280d2d163fe57b8290
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-08-03 22:46:55 -0300

    chore: add request validator config

---
+++ Testeger/Api/v1/Requests/CreateProject/CreateProjectRequestValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Testeger.Api.v1.Requests.CreateProject;
+
+public class CreateProjectRequestValidator : AbstractValidator<CreateProjectRequest>
+{
+    public CreateProjectRequestValidator()
+    {
+        RuleFor(r => r.Name)
+            .NotEmpty()
+            .MaximumLength(50);
+
+        RuleFor(r => r.UserId)
+            .NotEmpty()
+            .MaximumLength(36);
+    }
+}
