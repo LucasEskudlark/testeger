1d88ab06dfa2707a21736ca92490be50d85b4466
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-08-05 20:29:01 -0300

    chore: add mapping profiles

---
+++ Testeger/Testeger.Application/MappingProfiles/EnumerationMappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using AutoMapper.Extensions.EnumMapping;
+using ApiEnum = Testeger.Application.DTOs.Enumerations;
+using DomainEnum = Testeger.Domain.Enumerations;
+
+namespace Testeger.Application.MappingProfiles;
+
+public class EnumerationMappingProfile : Profile
+{
+    public EnumerationMappingProfile()
+    {
+        CreateMap<ApiEnum.PriorityLevel, DomainEnum.PriorityLevel>()
+            .ConvertUsingEnumMapping();
+
+        CreateMap<ApiEnum.RequestStatus, DomainEnum.RequestStatus>()
+            .ConvertUsingEnumMapping();
+
+        CreateMap<ApiEnum.RoleType, DomainEnum.RoleType>()
+            .ConvertUsingEnumMapping();
+
+        CreateMap<ApiEnum.TestCaseStatus, DomainEnum.TestCaseStatus>()
+            .ConvertUsingEnumMapping();
+    }
+}
