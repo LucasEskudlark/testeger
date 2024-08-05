359e10b35d41adfb2a96907bda52dc6da1478cdd
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-08-05 20:29:45 -0300

    feat: add endpoint

---
+++ Testeger/Api/Controllers/TestRequestsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Testeger.Application.DTOs.Requests.CreateTestRequest;
+using Testeger.Application.Services.TestRequest;
+
+namespace Testeger.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TestRequestsController : ControllerBase
+{
+    private readonly ITestRequestService _testRequestService;
+
+    public TestRequestsController(ITestRequestService testRequestService)
+    {
+        _testRequestService = testRequestService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateProjectAsync([FromBody] CreateTestRequestRequest request)
+    {
+        var response = await _testRequestService.CreateTestRequestAsync(request);
+
+        return Ok(response);
+    }
+}
