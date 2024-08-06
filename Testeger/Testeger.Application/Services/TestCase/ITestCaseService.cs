d60de98d54345c2f2c30525f83d69eb447da585f
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-08-06 20:04:47 -0300

    feat: add test case creation

---
+++ Testeger/Testeger.Application/Services/TestCase/ITestCaseService.cs
@@ -0,0 +1,9 @@
+﻿using Testeger.Application.DTOs.Requests.CreateTestCase;
+using Testeger.Application.DTOs.Responses.TestCase;
+
+namespace Testeger.Application.Services.TestCase;
+
+public interface ITestCaseService
+{
+    Task<CreateTestCaseResponse> CreateTestCaseAsync(CreateTestCaseRequest request);
+}
