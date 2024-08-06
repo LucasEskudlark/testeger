cf0369538ee9a6b3a4245f314474e4e928c2c38a
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-08-06 19:41:54 -0300

    chore: add mapping profile

---
+++ Testeger/Testeger.Application/MappingProfiles/TestCaseMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Testeger.Application.DTOs.Requests.Common;
+using Testeger.Application.DTOs.Requests.CreateTestCase;
+using Testeger.Application.DTOs.Responses.TestCase;
+using Testeger.Domain.Models.Entities;
+using Testeger.Domain.Models.ValueObjects;
+
+namespace Testeger.Application.MappingProfiles;
+
+public class TestCaseMappingProfile : Profile
+{
+    public TestCaseMappingProfile()
+    {
+        CreateMap<CreateTestCaseRequest, TestCase>()
+            .ForMember(t => t.CreatedByUserId,
+                opt => opt.MapFrom(r => r.UserId));
+        CreateMap<TestCase, CreateTestCaseResponse>();
+        CreateMap<TestCaseDetailsRequest, TestCaseDetails>();
+    }
+}
