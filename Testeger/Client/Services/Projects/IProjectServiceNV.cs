5a1950188a23deddad8d9d08daf1b46ea26b80ab
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-08-10 20:52:18 -0300

    refact: migrate project listing

--- Testeger/Client/Services/Projects/IProjectServiceNV.cs
+++ Testeger/Client/Services/Projects/IProjectServiceNV.cs
@@ -1,10 +1,16 @@
 ﻿using Testeger.Shared.DTOs.Requests.CreateProject;
+using Testeger.Shared.DTOs.Responses;
 using Testeger.Shared.DTOs.Responses.Project;
 
 namespace Testeger.Client.Services.Projects;
 
 public interface IProjectServiceNV
 {
+    public event Action? OnChange;
+    public event Action? OnProjectAdded;
+    public event Action? OnProjectDeleted;
+    public event Action? OnProjectUpdated;
     Task<CreateProjectResponse> CreateProjectAsync(CreateProjectRequest request);
     Task<GetProjectResponse> GetProjectByIdAsync(string id);
+    Task<PagedResponse<GetProjectResponse>> GetAllProjectsPagedAsync();
 }
