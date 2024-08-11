731dc2283a47d9a7cca030a2ac57476b076ccaf4
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-08-11 09:43:18 -0300

    chore: migrate breadcrumbhelper to client project

---
+++ Testeger/Client/Helpers/BreadCrumbHelper.cs
@@ -0,0 +1,8 @@
+﻿namespace Testeger.Client.Helpers;
+
+public static class BreadCrumbHelper
+{
+    public static string GetProjectUrl(string id) => $"/project/{id}";
+    public static string GetRequestUrl(string requestId) => $"/test-request/{requestId}";
+    public static string GetRequestText(int number) => $"Request n°{number}";
+}
