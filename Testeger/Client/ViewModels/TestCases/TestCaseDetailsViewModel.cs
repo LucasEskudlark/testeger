d92a18024ade2da4e5d450a796af1fc20bd3c347
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-08-12 19:18:58 -0300

    chore: add viewmodels

---
+++ Testeger/Client/ViewModels/TestCases/TestCaseDetailsViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Testeger.Client.ViewModels.TestCases;
+
+public class TestCaseDetailsViewModel
+{
+    public required string Description { get; set; }
+    public required string PreConditions { get; set; }
+    public required string Steps { get; set; }
+    public required string ExpectedResult { get; set; }
+    public required string Environment { get; set; }
+}
---
+++ Testeger/Client/ViewModels/TestCases/TestCaseHistoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+using Testeger.Shared.DTOs.Enumerations;
+
+namespace Testeger.Client.ViewModels.TestCases;
+
+public class TestCaseHistoryViewModel
+{
+    public required string ChangedByUserId { get; set; }
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public TestCaseStatus OldStatus { get; set; }
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public TestCaseStatus NewStatus { get; set; }
+    public DateTime ChangedDate { get; set; }
+}
