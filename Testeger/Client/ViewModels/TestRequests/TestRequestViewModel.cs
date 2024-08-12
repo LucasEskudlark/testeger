2c70d140f84bba723b1335158f4c5210976dd47d
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-08-12 18:43:30 -0300

    feat: add test request viewmodel

---
+++ Testeger/Client/ViewModels/TestRequests/TestRequestViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+using Testeger.Shared.DTOs.Enumerations;
+
+namespace Testeger.Client.ViewModels.TestRequests;
+
+public class TestRequestViewModel
+{
+    public required string Id { get; set; }
+    public int Number { get; set; }
+    public required string ProjectId { get; set; }
+    public required string CreatedByUserId { get; set; }
+    public required string Title { get; set; }
+    public required string Description { get; set; }
+    public required string StoryLink { get; set; }
+    public required string UserAssignedId { get; set; }
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public RequestStatus Status { get; set; }
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public PriorityLevel PriorityLevel { get; set; }
+    public DateTime DueDate { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime CompletedDate { get; set; }
+    public required IEnumerable<TestRequestHistoryViewModel> History { get; set; }
+}
