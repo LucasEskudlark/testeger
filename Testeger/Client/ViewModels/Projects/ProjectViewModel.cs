54266ea6e83985ae0c8222da866a429063c756d8
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-08-12 18:55:54 -0300

    feat: add project vm

---
+++ Testeger/Client/ViewModels/Projects/ProjectViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Testeger.Client.ViewModels.Projects;
+
+public class ProjectViewModel
+{
+    public required string Id { get; set; }
+    public required string Name { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public required string CreatedByUserId { get; set; }
+}
