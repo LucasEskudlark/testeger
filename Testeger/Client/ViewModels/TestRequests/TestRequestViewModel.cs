2d520f4009552dfda9d9e18efcd5bc0316c59c96
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-08-19 19:23:18 -0300

    chore: add validations

--- Testeger/Client/ViewModels/TestRequests/TestRequestViewModel.cs
+++ Testeger/Client/ViewModels/TestRequests/TestRequestViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 using Testeger.Shared.DTOs.Enumerations;
 
 namespace Testeger.Client.ViewModels.TestRequests;
@@ -9,14 +10,32 @@ public class TestRequestViewModel
     public int Number { get; set; }
     public required string ProjectId { get; set; }
     public required string CreatedByUserId { get; set; }
+
+    [Required(ErrorMessage = "Title is required")]
+    [MinLength(5, ErrorMessage = "Title must be at least 5 characters")]
+    [MaxLength(50, ErrorMessage = "Title must be at 50 characters at most")]
     public required string Title { get; set; }
+
+    [Required(ErrorMessage = "Description is required")]
+    [MinLength(5, ErrorMessage = "Title must be at least 5 characters")]
+    [MaxLength(1500, ErrorMessage = "Title must be at 1500 characters at most")]
     public required string Description { get; set; }
+
+    [Url(ErrorMessage = "Story Link must be a valid URL")]
     public required string StoryLink { get; set; }
     public required string UserAssignedId { get; set; }
+
+    [Required(ErrorMessage = "Request Status is required")]
+    [EnumDataType(typeof(RequestStatus))]
     [JsonConverter(typeof(JsonStringEnumConverter))]
     public RequestStatus Status { get; set; }
+
+    [Required(ErrorMessage = "Priority Level is required")]
+    [EnumDataType(typeof(PriorityLevel))]
     [JsonConverter(typeof(JsonStringEnumConverter))]
     public PriorityLevel PriorityLevel { get; set; }
+
+    [Required(ErrorMessage = "Due Date is required")]
     public DateTime DueDate { get; set; }
     public DateTime CreatedDate { get; set; }
     public DateTime CompletedDate { get; set; }
