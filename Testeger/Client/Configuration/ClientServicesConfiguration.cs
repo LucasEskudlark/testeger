89ee95e026cc8d22a0427460a03c94857d165ddc
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-08-26 18:43:57 -0300

    refact: create extension methods for configuration

---
+++ Testeger/Client/Configuration/ClientServicesConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Testeger.Client.Services.Images;
+using Testeger.Client.Services.Notifications;
+using Testeger.Client.Services.Projects;
+using Testeger.Client.Services.TestCaseResults;
+using Testeger.Client.Services.TestCases;
+using Testeger.Client.Services.TestRequests;
+
+namespace Testeger.Client.Configuration;
+
+public static class ClientServicesConfiguration
+{
+    public static void AddClientServices(this IServiceCollection services)
+    {
+        services.AddScoped<IProjectServiceNV, ProjectServiceNV>();
+        services.AddScoped<ITestRequestServiceNV, TestRequestServiceNV>();
+        services.AddScoped<ITestCaseServiceNV, TestCaseServiceNV>();
+        services.AddScoped<ITestCaseResultService, TestCaseResultService>();
+        services.AddScoped<IImageService, ImageService>();
+        services.AddScoped<INotificationService, NotificationService>();
+    }
+}
--- Testeger/Client/Program.cs
+++ Testeger/Client/Program.cs
@@ -2,11 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Radzen;
 using Testeger.Client;
-using Testeger.Client.Services.Images;
-using Testeger.Client.Services.Projects;
-using Testeger.Client.Services.TestCaseResults;
-using Testeger.Client.Services.TestCases;
-using Testeger.Client.Services.TestRequests;
+using Testeger.Client.Configuration;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -16,21 +12,7 @@ builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnC
 builder.Configuration.AddJsonFile($"appsettings.{builder.HostEnvironment.Environment}.json", optional: true, reloadOnChange: true);
 
 builder.Services.AddRadzenComponents();
-
-builder.Services.AddScoped<IProjectServiceNV, ProjectServiceNV>();
-builder.Services.AddScoped<ITestRequestServiceNV, TestRequestServiceNV>();
-builder.Services.AddScoped<ITestCaseServiceNV, TestCaseServiceNV>();
-builder.Services.AddScoped<ITestCaseResultService, TestCaseResultService>();
-builder.Services.AddScoped<IImageService, ImageService>();
-
-var baseAddress = builder.Configuration["ApiSettings:BaseAddress"]
-    ?? throw new InvalidOperationException("BaseAddress is not configured");
-
-builder.Services.AddScoped(sp =>
-    new HttpClient
-    {
-        BaseAddress = new Uri(baseAddress)
-    });
-builder.Services.AddHttpClient();
+builder.Services.AddClientServices();
+builder.Services.SetupHttpClient(builder.Configuration);
 
 await builder.Build().RunAsync();
