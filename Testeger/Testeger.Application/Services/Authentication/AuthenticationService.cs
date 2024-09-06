de398683eed4bcfdbc43de0f13a7bc808e5624e4
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-09-06 08:17:43 -0300

    feat: add authentication service

---
+++ Testeger/Testeger.Application/Services/Authentication/AuthenticationService.cs
@@ -0,0 +1,179 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Testeger.Application.Exceptions;
+using Testeger.Application.Services.Token;
+using Testeger.Application.Settings;
+using Testeger.Domain.Models.Entities;
+using Testeger.Shared.DTOs.Common;
+using Testeger.Shared.DTOs.Requests.Authentication.Login;
+using Testeger.Shared.DTOs.Requests.Authentication.Register;
+
+namespace Testeger.Application.Services.Authentication;
+
+public class AuthenticationService : IAuthenticationService
+{
+    private readonly ITokenService _tokenService;
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly JwtSettings _jwtSettings;
+
+    public AuthenticationService(
+        ITokenService tokenService,
+        UserManager<ApplicationUser> userManager,
+        RoleManager<IdentityRole> roleManager,
+        IOptions<JwtSettings> jwtSettings)
+    {
+        _tokenService = tokenService;
+        _userManager = userManager;
+        _roleManager = roleManager;
+        _jwtSettings = jwtSettings.Value;
+    }
+
+    public async Task AddRoleToUserAsync(string email, string roleName)
+    {
+        var user = await _userManager.FindByEmailAsync(email)
+            ?? throw new NotFoundException("User not found");
+
+        var result = await _userManager.AddToRoleAsync(user, roleName);
+
+        if (!result.Succeeded) throw new InvalidOperationException($"Unable to add user to role {roleName}");
+    }
+
+    public async Task<TokenDto> AuthenticateUserAsync(UserLoginRequest request)
+    {
+        var user = await _userManager.FindByNameAsync(request.Username);
+
+        if (user == null || !await _userManager.CheckPasswordAsync(user, request.Password))
+        {
+            return new TokenDto();
+        }
+
+        var authClaims = await GetUserClaims(user);
+
+        var token = _tokenService.GenerateAccessToken(authClaims);
+
+        var refreshToken = _tokenService.GenerateRefreshToken();
+        user.RefreshTokenExpiryTime = DateTime.UtcNow.AddMinutes(_jwtSettings.RefreshTokenValidityInMinutes);
+        user.RefreshToken = refreshToken;
+        await _userManager.UpdateAsync(user);
+
+        return new TokenDto
+        {
+            Token = new JwtSecurityTokenHandler().WriteToken(token),
+            RefreshToken = refreshToken,
+            Expiration = token.ValidTo
+        };
+    }
+
+    public async Task CreateRoleAsync(string roleName)
+    {
+        var roleExist = await _roleManager.RoleExistsAsync(roleName);
+
+        if (roleExist)
+        {
+            throw new InvalidOperationException($"Role {roleName} already exists");
+        }
+
+        var roleResult = await _roleManager.CreateAsync(new IdentityRole(roleName));
+
+        if (!roleResult.Succeeded)
+        {
+            throw new InvalidOperationException($"Error while adding the role {roleName}");
+        }
+    }
+
+    public async Task<TokenDto> RefreshTokenAsync(TokenDto request)
+    {
+        var principal = _tokenService.GetPrincipalFromExpiredToken(request.Token!)
+            ?? throw new InvalidOperationException("Invalid access/refresh token");
+
+        var username = principal.Identity?.Name;
+
+        var user = username is not null
+            ? await _userManager.FindByNameAsync(username!)
+            : throw new NotFoundException($"Identity not found for the token informed"); ;
+
+        if (user == null || user.RefreshToken != request.RefreshToken || user.RefreshTokenExpiryTime <= DateTime.Now)
+        {
+            throw new InvalidOperationException("Invalid access/refresh token");
+        }
+
+        var newAccessToken = _tokenService.GenerateAccessToken(
+            principal.Claims.ToList());
+
+        var newRefreshToken = _tokenService.GenerateRefreshToken();
+
+        user.RefreshToken = newRefreshToken;
+        await _userManager.UpdateAsync(user);
+
+        return new TokenDto
+        {
+            Token = new JwtSecurityTokenHandler().WriteToken(newAccessToken),
+            RefreshToken = newRefreshToken
+        };
+    }
+
+    public async Task RegisterUserAsync(UserRegisterRequest request)
+    {
+        await ValidateUserNameAndEmailAsync(request);
+
+        var user = new ApplicationUser
+        {
+            UserName = request.UserName,
+            Email = request.Email,
+            SecurityStamp = Guid.NewGuid().ToString(),
+        };
+
+        var userCreationResult = await _userManager.CreateAsync(user);
+
+        if (!userCreationResult.Succeeded)
+        {
+            throw new InvalidOperationException($"Error while creating user.");
+        }
+    }
+
+    public async Task RevokeAsync(string username)
+    {
+        var user = await _userManager.FindByNameAsync(username)
+            ?? throw new NotFoundException("Invalid user name");
+
+        user.RefreshToken = null;
+
+        await _userManager.UpdateAsync(user);
+    }
+
+    private async Task<List<Claim>> GetUserClaims(ApplicationUser user)
+    {
+        var userRoles = await _userManager.GetRolesAsync(user);
+
+        var authClaims = new List<Claim>
+        {
+            new(ClaimTypes.Name, user.UserName!),
+            new(ClaimTypes.Email, user.Email!),
+            new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+        };
+
+        authClaims.AddRange(userRoles.Select(role => new Claim(ClaimTypes.Role, role)));
+
+        return authClaims;
+    }
+
+    private async Task ValidateUserNameAndEmailAsync(UserRegisterRequest request)
+    {
+        var userByName = await _userManager.FindByNameAsync(request.UserName);
+
+        if (userByName is not null)
+        {
+            throw new InvalidOperationException($"Could not create user. User with name {request.UserName} already exists");
+        }
+
+        var userByEmail = await _userManager.FindByEmailAsync(request.Email);
+
+        if (userByEmail is not null)
+        {
+            throw new InvalidOperationException($"Could not create user. User with email {request.Email} already exists");
+        }
+    }
+}
---
+++ Testeger/Testeger.Application/Services/Authentication/IAuthenticationService.cs
@@ -0,0 +1,15 @@
+﻿using Testeger.Shared.DTOs.Common;
+using Testeger.Shared.DTOs.Requests.Authentication.Login;
+using Testeger.Shared.DTOs.Requests.Authentication.Register;
+
+namespace Testeger.Application.Services.Authentication;
+
+public interface IAuthenticationService
+{
+    Task<TokenDto> AuthenticateUserAsync(UserLoginRequest request);
+    Task RegisterUserAsync(UserRegisterRequest request);
+    Task<TokenDto> RefreshTokenAsync(TokenDto request);
+    Task RevokeAsync(string username);
+    Task CreateRoleAsync(string roleName);
+    Task AddRoleToUserAsync(string email, string roleName);
+}
