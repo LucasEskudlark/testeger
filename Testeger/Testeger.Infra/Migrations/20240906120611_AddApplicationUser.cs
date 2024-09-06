fff62e3e003f320fda668b0c9099993af4261d0f
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-09-06 09:21:12 -0300

    feat: add app user to db context

---
+++ Testeger/Testeger.Infra/Migrations/20240906120611_AddApplicationUser.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Testeger.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddApplicationUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ChangedDate",
+                table: "TestRequestHistory",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(2024, 9, 6, 12, 6, 10, 701, DateTimeKind.Utc).AddTicks(8548),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValue: new DateTime(2024, 9, 5, 20, 44, 11, 967, DateTimeKind.Utc).AddTicks(4018));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedDate",
+                table: "TestRequest",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(2024, 9, 6, 12, 6, 10, 700, DateTimeKind.Utc).AddTicks(9918),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValue: new DateTime(2024, 9, 5, 20, 44, 11, 966, DateTimeKind.Utc).AddTicks(5474));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ChangedDate",
+                table: "TestCaseHistory",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(2024, 9, 6, 12, 6, 10, 705, DateTimeKind.Utc).AddTicks(3833),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValue: new DateTime(2024, 9, 5, 20, 44, 11, 970, DateTimeKind.Utc).AddTicks(6536));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedDate",
+                table: "TestCase",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(2024, 9, 6, 12, 6, 10, 702, DateTimeKind.Utc).AddTicks(2042),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValue: new DateTime(2024, 9, 5, 20, 44, 11, 967, DateTimeKind.Utc).AddTicks(7356));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Project",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(2024, 9, 6, 12, 6, 10, 700, DateTimeKind.Utc).AddTicks(6274),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValue: new DateTime(2024, 9, 5, 20, 44, 11, 966, DateTimeKind.Utc).AddTicks(2141));
+
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "AspNetUsers",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExpiryTime",
+                table: "AspNetUsers",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpiryTime",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ChangedDate",
+                table: "TestRequestHistory",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(2024, 9, 5, 20, 44, 11, 967, DateTimeKind.Utc).AddTicks(4018),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValue: new DateTime(2024, 9, 6, 12, 6, 10, 701, DateTimeKind.Utc).AddTicks(8548));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedDate",
+                table: "TestRequest",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(2024, 9, 5, 20, 44, 11, 966, DateTimeKind.Utc).AddTicks(5474),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValue: new DateTime(2024, 9, 6, 12, 6, 10, 700, DateTimeKind.Utc).AddTicks(9918));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ChangedDate",
+                table: "TestCaseHistory",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(2024, 9, 5, 20, 44, 11, 970, DateTimeKind.Utc).AddTicks(6536),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValue: new DateTime(2024, 9, 6, 12, 6, 10, 705, DateTimeKind.Utc).AddTicks(3833));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedDate",
+                table: "TestCase",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(2024, 9, 5, 20, 44, 11, 967, DateTimeKind.Utc).AddTicks(7356),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValue: new DateTime(2024, 9, 6, 12, 6, 10, 702, DateTimeKind.Utc).AddTicks(2042));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Project",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(2024, 9, 5, 20, 44, 11, 966, DateTimeKind.Utc).AddTicks(2141),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValue: new DateTime(2024, 9, 6, 12, 6, 10, 700, DateTimeKind.Utc).AddTicks(6274));
+        }
+    }
+}
