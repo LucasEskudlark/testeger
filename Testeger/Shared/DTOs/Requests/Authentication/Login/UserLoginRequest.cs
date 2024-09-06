9748d757a4562de330ddb0e9191d267e98e91251
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-09-06 08:17:36 -0300

    chore: add dtos

---
+++ Testeger/Shared/DTOs/Requests/Authentication/Login/UserLoginRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Testeger.Shared.DTOs.Requests.Authentication.Login;
+
+public class UserLoginRequest
+{
+    public required string Username { get; set; }
+    public required string Password { get; set; }
+}
---
+++ Testeger/Shared/DTOs/Requests/Authentication/Register/UserRegisterRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace Testeger.Shared.DTOs.Requests.Authentication.Register;
+
+public class UserRegisterRequestValidator : AbstractValidator<UserRegisterRequest>
+{
+    public UserRegisterRequestValidator()
+    {
+        RuleFor(x => x.UserName)
+            .NotEmpty();
+
+        RuleFor(x => x.Password)
+            .NotEmpty();
+
+        RuleFor(x => x.Email)
+            .EmailAddress()
+            .NotEmpty();
+    }
+}
