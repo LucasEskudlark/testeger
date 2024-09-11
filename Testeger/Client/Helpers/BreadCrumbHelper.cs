ec7c30e0305521c96adef87e2f47b914302ca9e4
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-09-11 18:45:15 -0300

    feat: add policy to attributes

--- Testeger/Client/Helpers/BreadCrumbHelper.cs
+++ Testeger/Client/Helpers/BreadCrumbHelper.cs
@@ -3,6 +3,6 @@
 public static class BreadCrumbHelper
 {
     public static string GetProjectUrl(string id) => $"/project/{id}";
-    public static string GetRequestUrl(string requestId) => $"/test-request/{requestId}";
+    public static string GetRequestUrl(string projectId, string requestId) => $"/project/{projectId}/test-request/{requestId}";
     public static string GetRequestText(int number) => $"Request n°{number}";
 }
