9cb49148a17fe0108b939fefa79b8eef298f938c
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-09-17 21:46:05 -0300

    feat: add dtos

---
+++ Testeger/Shared/DTOs/Requests/ConfirmInvitation/ConfirmInvitationRequestValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Testeger.Shared.DTOs.Requests.ConfirmInvitation;
+
+public class ConfirmInvitationRequestValidator : AbstractValidator<ConfirmInvitationRequest>
+{
+    public ConfirmInvitationRequestValidator()
+    {
+        RuleFor(r => r.Token)
+            .NotEmpty();
+    }
+}
