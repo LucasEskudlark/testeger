fde1ce8752fc69b5f49b57f1c45da3bda2d1c987
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-09-17 21:45:43 -0300

    chore: change invitation token data size

---
+++ Testeger/Testeger.Infra/Migrations/20240918004510_ChangeInvitationTokenSize.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Testeger.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeInvitationTokenSize : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ChangedDate",
+                table: "TestRequestHistory",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(2024, 9, 18, 0, 45, 10, 492, DateTimeKind.Utc).AddTicks(7578),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValue: new DateTime(2024, 9, 17, 0, 15, 51, 28, DateTimeKind.Utc).AddTicks(3032));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedDate",
+                table: "TestRequest",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(2024, 9, 18, 0, 45, 10, 491, DateTimeKind.Utc).AddTicks(1602),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValue: new DateTime(2024, 9, 17, 0, 15, 51, 26, DateTimeKind.Utc).AddTicks(7319));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ChangedDate",
+                table: "TestCaseHistory",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(2024, 9, 18, 0, 45, 10, 497, DateTimeKind.Utc).AddTicks(813),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValue: new DateTime(2024, 9, 17, 0, 15, 51, 34, DateTimeKind.Utc).AddTicks(3136));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedDate",
+                table: "TestCase",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(2024, 9, 18, 0, 45, 10, 493, DateTimeKind.Utc).AddTicks(3307),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValue: new DateTime(2024, 9, 17, 0, 15, 51, 28, DateTimeKind.Utc).AddTicks(9309));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Project",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(2024, 9, 18, 0, 45, 10, 490, DateTimeKind.Utc).AddTicks(2465),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValue: new DateTime(2024, 9, 17, 0, 15, 51, 25, DateTimeKind.Utc).AddTicks(8097));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "InvitationToken",
+                table: "Invitations",
+                type: "varchar(64)",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(128)",
+                oldMaxLength: 128)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ChangedDate",
+                table: "TestRequestHistory",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(2024, 9, 17, 0, 15, 51, 28, DateTimeKind.Utc).AddTicks(3032),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValue: new DateTime(2024, 9, 18, 0, 45, 10, 492, DateTimeKind.Utc).AddTicks(7578));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedDate",
+                table: "TestRequest",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(2024, 9, 17, 0, 15, 51, 26, DateTimeKind.Utc).AddTicks(7319),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValue: new DateTime(2024, 9, 18, 0, 45, 10, 491, DateTimeKind.Utc).AddTicks(1602));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ChangedDate",
+                table: "TestCaseHistory",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(2024, 9, 17, 0, 15, 51, 34, DateTimeKind.Utc).AddTicks(3136),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValue: new DateTime(2024, 9, 18, 0, 45, 10, 497, DateTimeKind.Utc).AddTicks(813));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedDate",
+                table: "TestCase",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(2024, 9, 17, 0, 15, 51, 28, DateTimeKind.Utc).AddTicks(9309),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValue: new DateTime(2024, 9, 18, 0, 45, 10, 493, DateTimeKind.Utc).AddTicks(3307));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Project",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(2024, 9, 17, 0, 15, 51, 25, DateTimeKind.Utc).AddTicks(8097),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValue: new DateTime(2024, 9, 18, 0, 45, 10, 490, DateTimeKind.Utc).AddTicks(2465));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "InvitationToken",
+                table: "Invitations",
+                type: "varchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(64)",
+                oldMaxLength: 64)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
