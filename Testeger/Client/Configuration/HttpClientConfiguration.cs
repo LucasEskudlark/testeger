47e9c04df4a846a39fee801ea8681af6b4c51b6a
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-09-28 20:50:09 -0300

    fix: add json header to client

--- Testeger/Client/Configuration/HttpClientConfiguration.cs
+++ Testeger/Client/Configuration/HttpClientConfiguration.cs
@@ -1,14 +1,20 @@
-﻿namespace Testeger.Client.Configuration;
+﻿using System.Net.Http.Headers;
+
+namespace Testeger.Client.Configuration;
 
 public static class HttpClientConfiguration
 {
     public static void SetupHttpClient(this IServiceCollection services, string baseAddress)
     {
         services.AddScoped(sp =>
-            new HttpClient
+        {
+            var client = new HttpClient
             {
                 BaseAddress = new Uri(baseAddress)
-            });
+            };
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            return client;
+        });
 
         services.AddHttpClient();
     }
