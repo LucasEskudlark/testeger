be97aff230a560ab4af0e6fd92d2ae5e9789a6aa
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-09-29 10:24:50 -0300

    chore: remove appsettings

--- Testeger/Client/Program.cs
+++ Testeger/Client/Program.cs
@@ -14,8 +14,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-builder.Configuration.AddJsonFile($"appsettings.{builder.HostEnvironment.Environment}.json", optional: true, reloadOnChange: true);
 
 ConfigurationData configData = new();
 try
