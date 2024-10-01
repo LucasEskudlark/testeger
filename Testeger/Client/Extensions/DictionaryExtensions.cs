98ef68a4178e4047741bc47397cbbfba50902d51
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-10-01 19:44:19 -0300

    fix: test case metrics

--- Testeger/Client/Extensions/DictionaryExtensions.cs
+++ Testeger/Client/Extensions/DictionaryExtensions.cs
@@ -27,4 +27,28 @@ public static class DictionaryExtensions
             return 0.0;
         }
     }
+
+    public static string GetTestCaseTimeByStatus(this Dictionary<TestCaseStatus, TimeSpan> dictionary, TestCaseStatus status)
+    {
+        if (dictionary.TryGetValue(status, out TimeSpan timeSpan))
+        {
+            return timeSpan.ToCustomFormat();
+        }
+        else
+        {
+            return new TimeSpan(0, 0, 0).ToCustomFormat();
+        }
+    }
+
+    public static double GetTestCaseTimeByStatusForChart(this Dictionary<TestCaseStatus, TimeSpan> dictionary, TestCaseStatus status)
+    {
+        if (dictionary.TryGetValue(status, out TimeSpan timeSpan))
+        {
+            return Math.Round(timeSpan.TotalHours, 2);
+        }
+        else
+        {
+            return 0.0;
+        }
+    }
 }
