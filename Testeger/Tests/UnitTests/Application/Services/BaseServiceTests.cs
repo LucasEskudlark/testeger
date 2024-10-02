5c324bf51f0490a4ee8c9f4275f6f7fb0a8d7e41
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-10-01 23:49:56 -0300

    feat: add base services and test request test class

---
+++ Testeger/Tests/UnitTests/Application/Services/BaseServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using AutoFixture;
+using AutoMapper;
+using Moq;
+using Testeger.Infra.UnitOfWork;
+
+namespace UnitTests.Application.Services;
+
+public class BaseServiceTests
+{
+    protected readonly Mock<IUnitOfWork> _unitOfWork;
+    protected readonly Mock<IMapper> _mapper;
+    protected readonly Fixture _fixture = new ();
+
+    public BaseServiceTests()
+    {
+        _mapper = new Mock<IMapper>();
+        _unitOfWork = new Mock<IUnitOfWork>();
+    }
+}
---
+++ Testeger/Tests/UnitTests/Application/Services/TestRequest/TestRequestServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using Moq;
+using Testeger.Shared.DTOs.Responses.TestRequest;
+using DomainTestRequest = Testeger.Domain.Models.Entities.TestRequest;
+namespace UnitTests.Application.Services.TestRequest;
+
+public class TestRequestServiceTests : TestRequestServiceTestBase
+{
+    private const string FakeRequestId = "FakeId";
+
+    [Fact]
+    public async Task GetTestRequestByIdAsync_GivenExistingId_ShouldReturnExpectedResponse()
+    {
+        var domainTestRequest = new DomainTestRequest { Id = FakeRequestId, CreatedByUserId = string.Empty };
+        var expectedResponse = _fixture.Build<GetTestRequestResponse>().With(x => x.Id, FakeRequestId).Create();
+
+        
+        _unitOfWork.Setup(uow => uow.TestRequestRepository.GetByIdAsync(FakeRequestId))
+            .ReturnsAsync(domainTestRequest);
+
+         _mapper.Setup(m => m.Map<GetTestRequestResponse>(domainTestRequest))
+            .Returns(expectedResponse);
+
+        var result = await _testRequestService.GetTestRequestByIdAsync(FakeRequestId);
+
+        result.Should().BeEquivalentTo(expectedResponse);
+    }
+}
